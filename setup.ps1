67bea212f964057396d9ad6b06117845b725087e
SuperDragon777 <Vadim.muromtsev@gmail.com>
2026-04-13 18:59:26 +0300

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,43 @@
+echo "Starting setup script..."
+
+$choice = Read-Host "Do you want to run installation? (y/n)"
+if ($choice -eq 'y') {
+    # install scoop
+    if (-not (Get-Command scoop -ErrorAction SilentlyContinue)) {
+        echo "Scoop not found, installing..."
+        Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+        Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+    } else {
+        echo "Scoop already installed, skipping installation."
+        scoop update
+    }
+
+    # install utils
+    echo "Installing utils..."
+    scoop install bat
+    scoop update bat
+    scoop install fastfetch
+    scoop update fastfetch
+    scoop install nano
+    scoop update nano
+    scoop install python
+    scoop update python
+    scoop install git
+    scoop update git
+    scoop install winget
+    scoop update winget
+    scoop install vscodium
+    scoop update vscodium
+    scoop install micro
+    scoop update micro
+} else {
+    echo "Skipping installation, proceeding to user part..."
+}
+
+# User part
+Clear-Host
+echo "Welcome, $env:USERNAME (SuperDragon777)"
+fastfetch
+
+echo "Script done, exiting"
+exit 0
